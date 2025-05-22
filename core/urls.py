9481e7f0b172bbcce5559809d9096ff1e358bc91
Elias Ovalle <remontesinoo22@ul.edu.co>
2025-05-22 15:51:34 -0500

    Prueba
    git

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('registro/paciente/', views.paciente_registro, name='paciente_registro'),
     path('medico/inicio/', views.medico_home, name='medico_home'),
     path('paciente/inicio/', views.paciente_home, name='paciente_home'),
+    #pasiente
     
 ]
